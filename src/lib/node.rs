a0fd0dc03689939d9c281d17b143b2abef681b98
AtsukiTak <takatomgoo@gmail.com>
2018-07-19 15:57:32 +0900

    add Node::recv_block function

--- src/lib/node.rs
+++ src/lib/node.rs
@@ -1,9 +1,10 @@
 use bitcoin::network::{message::NetworkMessage, message_blockdata::{GetBlocksMessage, InvType, Inventory},
                        serialize::BitcoinHash};
 use bitcoin::util::hash::Sha256dHash;
+use bitcoin::blockdata::block::Block;
 
 use connection::Connection;
-use blockchain::BlockChain;
+use blockchain::{BlockChain, StoredBlock};
 use error::Error;
 
 pub struct Node
@@ -13,22 +14,6 @@ pub struct Node
 
 impl Node
 {
-    /// Process incoming `inv` message.
-    /// `inv` message often be sent as response of `getblocks` message.
-    /// After we receive `inv` message, we send `getdata` message.
-    fn recv_inv(&mut self, invs: Vec<Inventory>, peer: &mut Connection)
-    {
-        if !check_invs(invs.as_slice(), &self.blockchain) {
-            warn!("Peer {:?} send us unwanted inventory. So we disconnect.", peer);
-            peer.disconnect();
-            return;
-        }
-
-        for inv in invs.iter() {
-            info!("Process incoming inventory : {:?}", inv);
-        }
-    }
-
     /// Send `getblocks` message to given `peer`.
     /// When we start, we need to send `getblocks` message first and then,
     /// we receive `inv` message as response.
@@ -40,10 +25,38 @@ impl Node
         peer.send_msg(network_msg)
     }
 
+    /// Process incoming `inv` message.
+    /// `inv` message often be sent as response of `getblocks` message.
+    /// After we receive `inv` message, we send `getdata` message.
+    fn recv_inv(&self, invs: Vec<Inventory>, peer: &mut Connection) -> Result<(), Error>
+    {
+        // Check received invs all are valid.
+        if !check_invs(invs.as_slice(), &self.blockchain) {
+            warn!("Peer {:?} send us unwanted inventory. So we disconnect.", peer);
+            peer.disconnect();
+            return Ok(());
+        }
+
+        self.request_data(invs, peer)
+    }
+
+    /// Send `getdata` message to given `peer`.
     fn request_data(&self, invs: Vec<Inventory>, peer: &mut Connection) -> Result<(), Error>
     {
         let msg = NetworkMessage::GetData(invs);
         peer.send_msg(msg)
+    }
+
+    fn recv_block(&mut self, block: Block, peer: &mut Connection) -> Result<(), Error>
+    {
+        info!("Process incoming block");
+        match self.blockchain.try_add(StoredBlock::new(block)) {
+            Ok(_) => Ok(()),
+            Err(_) => {
+                peer.disconnect();
+                Ok(())
+            },
+        }
     }
 }
 
