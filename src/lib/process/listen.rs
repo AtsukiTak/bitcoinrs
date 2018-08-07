1b0471e413efae4674029cb30720d7af5240b4a2
AtsukiTak <takatomgoo@gmail.com>
2018-08-07 18:51:04 +0900

    impl listen_new_block

--- src/lib/process/listen.rs
+++ src/lib/process/listen.rs
@@ -1,20 +1,78 @@
-use connection::Connection;
-use super::ProcessError;
+use bitcoin::blockdata::block::Block;
+use bitcoin::network::serialize::BitcoinHash;
+use futures::{Future, stream::{unfold, Stream}};
 
-pub fn listen_new_block(conn: Connection) -> impl Stream<Item = Block, Error = ProcessError>
+use connection::{Connection, IncomingMessage};
+use blockchain::{BlockChain, BlockChainMut, BlockData};
+use super::{getblocks, getheaders, ProcessError};
+
+pub fn listen_new_block(
+    conn: Connection,
+    block_chain: BlockChainMut,
+) -> impl Stream<Item = (BlockChain, Vec<Block>), Error = ProcessError>
 {
+    unfold((conn, block_chain), |(conn, block_chain)| {
+        let f = listen_single_process(conn, block_chain).map(|(conn, block_chain, blocks)| {
+            let chain = block_chain.freeze();
+            ((chain, blocks), (conn, block_chain))
+        });
+        Some(f)
+    })
 }
 
-fn wait_recv_block(conn: Connection) {
-    conn.recv_msg().and_then(|(msg, conn)| {
-        match msg {
-            // If we use "standard block relay", peer sends "inv" message first.
-            // Or even if we have signalled "sendheaders", peer may send "inv" message first.
-            IncomingMessage::Inv(invs) => Hoge,
+fn listen_single_process(
+    conn: Connection,
+    mut block_chain: BlockChainMut,
+) -> impl Future<Item = (Connection, BlockChainMut, Vec<Block>), Error = ProcessError>
+{
+    let locator_hashes = block_chain.locator_blocks().map(|b| b.bitcoin_hash()).collect();
+    conn.recv_msg()
+        .map_err(|e| {
+            error!("{:?}", e);
+            ProcessError::ConnectionError
+        })
+        .and_then(|(msg, conn)| {
+            match msg {
+                // If we use "standard block relay", peer sends "inv" message first.
+                // Or even if we have signalled "sendheaders", peer still may send "inv" message first.
+                IncomingMessage::Inv(_invs) => Ok(conn),
 
-            // If we have signalled "sendheaders", we may use "direct headers announcement".
-            // In that case, peer may send "headers" message instead of "inv" message.
-            IncomingMessage::Headers(headers) => hoge,
-        }
-    })
+                // If we have signalled "sendheaders", we may use "direct headers announcement".
+                // In that case, peer may send "headers" message instead of "inv" message.
+                // For our current implementation, we don't use this feature so we just disconnect if
+                // we received headers message first.
+                IncomingMessage::Headers(_) => {
+                    warn!("Expect inv message but receive headers message.");
+                    Err(ProcessError::Misbehavior(conn))
+                },
+
+                IncomingMessage::Block(_) => {
+                    warn!("Expect inv message but receive block message.");
+                    Err(ProcessError::Misbehavior(conn))
+                },
+            }
+        })
+
+        // re-fetch headers newer than I have
+        .and_then(move |conn| getheaders(conn, locator_hashes)) // Future<Item = (Connection, Vec<BlockHeader>)>
+
+        // try to apply to internal blockchain
+        .and_then(move |(conn, headers)| {
+            for header in headers.iter() {
+                match block_chain.try_add(BlockData::new(header.clone())) {
+                    Ok(_) => {},
+                    Err(_e) => {
+                        warn!("Peer {} sends invalid header", conn);
+                        return Err(ProcessError::Misbehavior(conn));
+                    },
+                }
+            }
+            Ok((conn, block_chain, headers))
+        })
+
+        // getblocks
+        .and_then(|(conn, block_chain, headers)| {
+            let hashes = headers.iter().map(|h| h.bitcoin_hash()).collect();
+            getblocks(conn, hashes).map(|(conn, blocks)| (conn, block_chain, blocks))
+        })
 }
