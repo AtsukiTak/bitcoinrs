05d63245c7c6a0279d38321cb9b61edceed57b27
AtsukiTak <takatomgoo@gmail.com>
2018-08-12 19:34:50 +0900

    change a send/recv process of AsyncSocket

--- src/lib/socket.rs
+++ src/lib/socket.rs
@@ -1,13 +1,12 @@
 use std::{io::Cursor, net::SocketAddr};
 use bitcoin::network::{address::Address, constants::{magic, Network, SERVICES, USER_AGENT},
-                       encodable::ConsensusDecodable, message::{NetworkMessage, RawNetworkMessage},
+                       encodable::ConsensusDecodable, message::{CommandString, NetworkMessage, RawNetworkMessage},
                        serialize::{serialize, RawDecoder}, socket::Socket};
 use bitcoin::util::Error as BitcoinError;
+use bitcoin::util::hash::Sha256dHash;
 
-use futures::{Future, Sink, Stream};
-use tokio_codec::{Decoder, Encoder, Framed};
-use tokio::{net::TcpStream as AsyncTcpStream};
-use bytes::BytesMut;
+use futures::Future;
+use tokio::net::TcpStream as AsyncTcpStream;
 
 use error::Error;
 
@@ -91,7 +90,8 @@ impl ::std::fmt::Display for SyncSocket
  */
 pub struct AsyncSocket
 {
-    socket: Framed<AsyncTcpStream, BitcoinNetworkCodec>,
+    socket: AsyncTcpStream,
+    codec: BitcoinNetworkCodec,
     local_addr: Address,
     remote_addr: Address,
     user_agent: &'static str,
@@ -104,10 +104,12 @@ impl AsyncSocket
         AsyncTcpStream::connect(addr)
             .map_err(Error::from)
             .and_then(move |socket| {
+                debug!("Recv buffer size is {}", socket.recv_buffer_size().unwrap());
                 let local_addr = Address::new(&socket.local_addr()?, SERVICES);
                 let remote_addr = Address::new(&socket.peer_addr()?, SERVICES);
                 Ok(AsyncSocket {
-                    socket: BitcoinNetworkCodec::new(network).framed(socket),
+                    socket,
+                    codec: BitcoinNetworkCodec::new(network),
                     local_addr,
                     remote_addr,
                     user_agent: USER_AGENT,
@@ -132,32 +134,52 @@ impl AsyncSocket
 
     pub fn send_msg(self, msg: NetworkMessage) -> impl Future<Item = Self, Error = Error>
     {
-        let (socket, local_addr, remote_addr) = (self.socket, self.local_addr, self.remote_addr);
-        socket.send(msg).map(move |socket| {
-            AsyncSocket {
-                socket,
-                local_addr,
-                remote_addr,
-                user_agent: USER_AGENT,
-            }
-        })
+        debug!("Send a message {:?}", msg);
+        let serialized = self.codec.encode_inner(msg);
+        let (socket, codec, local_addr, remote_addr) = (self.socket, self.codec, self.local_addr, self.remote_addr);
+
+        ::tokio::io::write_all(socket, serialized)
+            .and_then(|(socket, _)| ::tokio::io::flush(socket))
+            .map_err(Error::from)
+            .map(move |socket| {
+                AsyncSocket {
+                    socket,
+                    codec,
+                    local_addr,
+                    remote_addr,
+                    user_agent: USER_AGENT,
+                }
+            })
     }
 
     pub fn recv_msg(self) -> impl Future<Item = (NetworkMessage, Self), Error = Error>
     {
-        let (socket, local_addr, remote_addr) = (self.socket, self.local_addr, self.remote_addr);
-        socket
-            .into_future()
-            .map_err(|(e, _socket)| Error::from(e))
-            .map(move |(maybe_msg, socket)| {
-                let msg = maybe_msg.unwrap(); // I'm not sure it could be `None`.
+        let (socket, codec, local_addr, remote_addr) = (self.socket, self.codec, self.local_addr, self.remote_addr);
+        let codec2 = codec.clone();
+        let header_buf: [u8; RAW_NETWORK_MESSAGE_HEADER_SIZE] = [0; RAW_NETWORK_MESSAGE_HEADER_SIZE];
+        ::tokio::io::read_exact(socket, header_buf)
+            .map_err(Error::from)
+            .and_then(move |(socket, bytes)| {
+                let header = codec.decode_msg_header(&bytes)?;
+                Ok((socket, header))
+            })
+            .and_then(|(socket, header)| {
+                let mut buf = Vec::with_capacity(header.payload_size as usize);
+                buf.resize(header.payload_size as usize, 0);
+                ::tokio::io::read_exact(socket, buf)
+                    .map_err(Error::from)
+                    .map(|(socket, bytes)| (socket, bytes, header))
+            })
+            .and_then(move |(socket, bytes, header)| {
+                let msg = codec2.decode_and_check_msg_payload(&bytes, &header)?;
                 let socket = AsyncSocket {
                     socket,
+                    codec: codec2,
                     local_addr,
                     remote_addr,
                     user_agent: USER_AGENT,
                 };
-                (msg, socket)
+                Ok((msg, socket))
             })
     }
 }
@@ -183,9 +205,19 @@ impl ::std::fmt::Display for AsyncSocket
 }
 
 
+#[derive(Copy, Clone)]
 struct BitcoinNetworkCodec
 {
     magic: u32,
+}
+
+const RAW_NETWORK_MESSAGE_HEADER_SIZE: usize = 24;
+
+struct RawNetworkMessageHeader
+{
+    command_name: CommandString,
+    payload_size: u32,
+    checksum: [u8; 4],
 }
 
 impl BitcoinNetworkCodec
@@ -195,65 +227,87 @@ impl BitcoinNetworkCodec
         BitcoinNetworkCodec { magic: magic(network) }
     }
 
-    fn decode_inner(&self, src: &[u8]) -> Result<Option<(NetworkMessage, usize)>, Error>
+    /// # Panic
+    /// If length of `src` is not 24 bytes.
+    fn decode_msg_header(&self, src: &[u8]) -> Result<RawNetworkMessageHeader, Error>
     {
-        let (res, consumed) = {
-            let mut decoder = RawDecoder::new(Cursor::new(src));
-            let res = RawNetworkMessage::consensus_decode(&mut decoder);
-            let cursor = decoder.into_inner();
-            let consumed = cursor.position();
-            (res, consumed)
-        };
-        match res {
-            Ok(msg) => {
-                if msg.magic == self.magic {
-                    Ok(Some((msg.payload, consumed as usize)))
-                } else {
-                    Err(Error::from(BitcoinError::BadNetworkMagic(self.magic, msg.magic)))
-                }
-            },
-            Err(BitcoinError::ByteOrder(_err)) => Ok(None),
-            Err(err) => Err(Error::from(err)),
+        assert!(src.len() == RAW_NETWORK_MESSAGE_HEADER_SIZE);
+
+        let mut decoder = RawDecoder::new(Cursor::new(src));
+
+        let magic = u32::consensus_decode(&mut decoder)?;
+        if magic != self.magic {
+            return Err(Error::from(BitcoinError::BadNetworkMagic(self.magic, magic)));
         }
+
+        let command_name = CommandString::consensus_decode(&mut decoder)?;
+        let payload_size = u32::consensus_decode(&mut decoder)?;
+        let checksum = <[u8; 4]>::consensus_decode(&mut decoder)?;
+
+        Ok(RawNetworkMessageHeader {
+            command_name,
+            payload_size,
+            checksum,
+        })
     }
 
-    fn encode_inner(&self, item: NetworkMessage) -> Result<Vec<u8>, Error>
+    /// # Panic
+    /// If length of `src` is not `header.payload_size`.
+    fn decode_and_check_msg_payload(
+        &self,
+        src: &[u8],
+        header: &RawNetworkMessageHeader,
+    ) -> Result<NetworkMessage, Error>
+    {
+        assert!(src.len() as u32 == header.payload_size);
+
+        let mut decoder = RawDecoder::new(Cursor::new(src));
+
+        // Check a checksum
+        let expected_checksum = sha2_checksum(&src);
+        if expected_checksum != header.checksum {
+            warn!("bad checksum");
+            return Err(Error::from(BitcoinError::ParseFailed));
+        }
+
+        let msg = match &header.command_name.0[..] {
+            "version" => NetworkMessage::Version(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "verack" => NetworkMessage::Verack,
+            "addr" => NetworkMessage::Addr(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "inv" => NetworkMessage::Inv(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "getdata" => NetworkMessage::GetData(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "notfound" => NetworkMessage::NotFound(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "getblocks" => NetworkMessage::GetBlocks(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "getheaders" => NetworkMessage::GetHeaders(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "mempool" => NetworkMessage::MemPool,
+            "block" => NetworkMessage::Block(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "headers" => NetworkMessage::Headers(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "getaddr" => NetworkMessage::GetAddr,
+            "ping" => NetworkMessage::Ping(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "pong" => NetworkMessage::Pong(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "tx" => NetworkMessage::Tx(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            "alert" => NetworkMessage::Alert(ConsensusDecodable::consensus_decode(&mut decoder)?),
+            cmd => {
+                warn!("unrecognized network command : {}", cmd);
+                return Err(Error::from(BitcoinError::ParseFailed));
+            },
+        };
+
+        Ok(msg)
+    }
+
+    fn encode_inner(&self, item: NetworkMessage) -> Vec<u8>
     {
         let msg = RawNetworkMessage {
             magic: self.magic,
             payload: item,
         };
-        Ok(serialize(&msg)?)
+        serialize(&msg).unwrap() // Never fail
     }
 }
 
-impl Decoder for BitcoinNetworkCodec
+fn sha2_checksum(data: &[u8]) -> [u8; 4]
 {
-    type Item = NetworkMessage;
-    type Error = Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error>
-    {
-        match self.decode_inner(&src) {
-            Ok(Some((msg, consumed))) => {
-                src.split_to(consumed);
-                Ok(Some(msg))
-            },
-            Ok(None) => Ok(None),
-            Err(e) => Err(e),
-        }
-    }
-}
-
-impl Encoder for BitcoinNetworkCodec
-{
-    type Item = NetworkMessage;
-    type Error = Error;
-
-    fn encode(&mut self, item: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error>
-    {
-        let vec = self.encode_inner(item)?;
-        dst.extend_from_slice(&vec);
-        Ok(())
-    }
+    let checksum = Sha256dHash::from_data(data);
+    [checksum[0], checksum[1], checksum[2], checksum[3]]
 }
