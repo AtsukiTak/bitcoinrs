694e58d5ff466361f9022d03cac0bb398fe14a29
AtsukiTak <takatomgoo@gmail.com>
2018-08-12 20:25:32 +0900

    remove SyncSocket

--- src/lib/socket.rs
+++ src/lib/socket.rs
@@ -10,80 +10,6 @@ use tokio_tcp::TcpStream as AsyncTcpStream;
 
 use error::Error;
 
-/*
- * SyncSocket
- */
-pub struct SyncSocket
-{
-    socket: Socket,
-    remote_addr: Address,
-    local_addr: Address,
-}
-
-impl SyncSocket
-{
-    pub fn open(addr: &SocketAddr, network: Network) -> Result<SyncSocket, Error>
-    {
-        let mut socket = Socket::new(network);
-        socket.connect(format!("{}", addr.ip()).as_str(), addr.port())?;
-        let remote_addr = socket.receiver_address()?;
-        let local_addr = socket.sender_address()?;
-        Ok(SyncSocket {
-            socket,
-            remote_addr,
-            local_addr,
-        })
-    }
-
-    pub fn remote_addr(&self) -> &Address
-    {
-        &self.remote_addr
-    }
-
-    pub fn local_addr(&self) -> &Address
-    {
-        &self.local_addr
-    }
-
-    pub fn user_agent(&self) -> &str
-    {
-        self.socket.user_agent.as_str()
-    }
-
-    pub fn send_msg(mut self, msg: NetworkMessage) -> Result<Self, Error>
-    {
-        debug!("Send new msg to {:?} : {:?}", self.remote_addr, msg);
-        self.socket.send_message(msg)?;
-        Ok(self)
-    }
-
-    pub fn recv_msg(mut self) -> Result<(NetworkMessage, Self), Error>
-    {
-        let msg = self.socket.receive_message()?;
-        debug!("Receive a new msg from {:?} : {:?}", self.remote_addr, msg);
-        Ok((msg, self))
-    }
-}
-
-impl ::std::fmt::Debug for SyncSocket
-{
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error>
-    {
-        write!(
-            f,
-            "SyncSocket {{ remote: {:?}, local: {:?} }}",
-            self.remote_addr, self.local_addr
-        )
-    }
-}
-
-impl ::std::fmt::Display for SyncSocket
-{
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error>
-    {
-        write!(f, "SyncSocket to peer {:?}", self.remote_addr.address)
-    }
-}
 
 /*
  * AsyncSocket
@@ -232,6 +158,8 @@ impl BitcoinNetworkCodec
     fn decode_msg_header(&self, src: &[u8]) -> Result<RawNetworkMessageHeader, Error>
     {
         assert!(src.len() == RAW_NETWORK_MESSAGE_HEADER_SIZE);
+
+        debug!("Decode message header");
 
         let mut decoder = RawDecoder::new(Cursor::new(src));
 
