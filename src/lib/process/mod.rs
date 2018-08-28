3b1aa1c6258ae5bf2248c25f581052ca64118e27
AtsukiTak <takatomgoo@gmail.com>
2018-08-28 13:49:05 +0900

    fix : remove initial_block_download process and add sync_blockchain process

--- src/lib/process/mod.rs
+++ src/lib/process/mod.rs
@@ -1,9 +1,9 @@
 mod process;
-mod ibd;
+mod sync;
 // mod listen;
 
 pub use self::process::{getblocks, getheaders, request_getblocks, request_getheaders, wait_recv_blocks,
                         wait_recv_headers};
 
-pub use self::ibd::initial_block_download;
+pub use self::sync::sync_blockchain;
 // pub use self::listen::listen_new_block;
