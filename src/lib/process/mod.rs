1178a3aba6482d45d2dfe041d318f40f04b0242d
AtsukiTak <takatomgoo@gmail.com>
2018-08-30 13:06:12 +0900

    refactor : process module

--- src/lib/process/mod.rs
+++ src/lib/process/mod.rs
@@ -1,9 +1,4 @@
+pub mod request;
 mod process;
-mod sync;
-// mod listen;
 
-pub use self::process::{getblocks, getheaders, request_getblocks, request_getheaders, wait_recv_blocks,
-                        wait_recv_headers};
-
-pub use self::sync::sync_blockchain;
-// pub use self::listen::listen_new_block;
+pub use self::process::sync_blockchain;
--- src/lib/process/process.rs
+++ src/lib/process/process.rs
@@ -1,130 +1,40 @@
-use bitcoin::network::{message_blockdata::{GetHeadersMessage, InvType, Inventory}, serialize::BitcoinHash};
-use bitcoin::blockdata::block::{Block, BlockHeader};
-use bitcoin::util::hash::Sha256dHash;
 use futures::future::{loop_fn, Future, Loop};
 
-use connection::{Connection, IncomingMessage, OutgoingMessage};
+use connection::Connection;
+use blockchain::BlockChain;
 use error::{Error, ErrorKind};
+use process::request::getheaders;
 
-/*
- * High level functions
- */
-
-pub fn getheaders(
+/// Sync given `BlockChain` with latest blockchain.
+/// This process only syncs `BlockHeader`.
+/// If you want `Block` as well, please use `process::getblocks` function.
+pub fn sync_blockchain(
     conn: Connection,
-    locator_hashes: Vec<Sha256dHash>,
-) -> impl Future<Item = (Connection, Vec<BlockHeader>), Error = Error>
+    block_chain: BlockChain,
+) -> impl Future<Item = (Connection, BlockChain), Error = Error>
 {
-    request_getheaders(conn, locator_hashes)
-        .and_then(wait_recv_headers)
-        .and_then(move |(conn, headers)| {
-            if headers.is_empty() {
-                info!("Peer {} sends empty headers message", conn);
-                return Err(Error::from(ErrorKind::MisbehaviorPeer(conn)));
-            }
-            Ok((conn, headers))
-        })
-}
+    const MAX_HEADERS_IN_MSG: usize = 2000;
 
-pub fn getblocks(
-    conn: Connection,
-    block_hashes: Vec<Sha256dHash>,
-) -> impl Future<Item = (Connection, Vec<Block>), Error = Error>
-{
-    let n_req_blocks = block_hashes.len();
-    request_getblocks(conn, block_hashes.clone())
-        .and_then(move |conn| wait_recv_blocks(conn, n_req_blocks))
-        .and_then(move |(conn, blocks)| {
-            let is_expected_blocks = blocks
-                .iter()
-                .zip(block_hashes.iter())
-                .all(|(block, hash)| block.bitcoin_hash() == *hash);
-            if !is_expected_blocks {
-                Err(Error::from(ErrorKind::MisbehaviorPeer(conn)))
-            } else {
-                Ok((conn, blocks))
-            }
-        })
-}
+    loop_fn(
+        (conn, block_chain), // Initial state
+        |(conn, mut block_chain)| {
+            let locator_hashes = block_chain.active_chain().locator_hashes_vec();
+            getheaders(conn, locator_hashes).and_then(move |(conn, headers)| {
+                info!("Received new {} headers", headers.len());
 
-/*
- * Low level functions
- */
+                let is_completed = headers.len() != MAX_HEADERS_IN_MSG;
 
-pub fn request_getheaders(
-    conn: Connection,
-    locator_hashes: Vec<Sha256dHash>,
-) -> impl Future<Item = Connection, Error = Error>
-{
-    let get_headers_msg = GetHeadersMessage::new(locator_hashes, Sha256dHash::default());
-    let msg = OutgoingMessage::GetHeaders(get_headers_msg);
-    conn.send_msg(msg)
-}
-
-pub fn wait_recv_headers(conn: Connection) -> impl Future<Item = (Connection, Vec<BlockHeader>), Error = Error>
-{
-    conn.recv_msg().then(move |res| {
-        match res? {
-            (IncomingMessage::Headers(hs), conn) => {
-                info!("Receive headers message");
-                let headers = hs.iter().map(|lone| lone.header).collect();
-                Ok((conn, headers))
-            },
-            (msg, conn) => {
-                info!("Receive unexpected message. Expected headers msg but receive {}", msg);
-                Err(Error::from(ErrorKind::MisbehaviorPeer(conn)))
-            },
-        }
-    })
-}
-
-
-pub fn request_getblocks(
-    conn: Connection,
-    block_hashes: Vec<Sha256dHash>,
-) -> impl Future<Item = Connection, Error = Error>
-{
-    let invs: Vec<_> = block_hashes
-        .iter()
-        .map(|hash| {
-            Inventory {
-                inv_type: InvType::Block,
-                hash: *hash,
-            }
-        })
-        .collect();
-    let msg = OutgoingMessage::GetData(invs);
-    conn.send_msg(msg)
-}
-
-
-pub fn wait_recv_blocks(
-    conn: Connection,
-    n_req_blocks: usize,
-) -> impl Future<Item = (Connection, Vec<Block>), Error = Error>
-{
-    loop_fn((conn, vec![], n_req_blocks), |(conn, mut blocks_buf, n_req_blocks)| {
-        conn.recv_msg().then(move |res| {
-            match res? {
-                // Receive "block" message
-                (IncomingMessage::Block(block), conn) => {
-                    info!("Receive a new block");
-                    blocks_buf.push(block);
-                    let n_rmn_blocks = n_req_blocks - 1;
-
-                    if n_rmn_blocks == 0 {
-                        Ok(Loop::Break((conn, blocks_buf)))
-                    } else {
-                        Ok(Loop::Continue((conn, blocks_buf, n_rmn_blocks)))
+                for header in headers {
+                    if let Err(_) = block_chain.try_add(header) {
+                        return Err(Error::from(ErrorKind::MisbehaviorPeer(conn)));
                     }
-                },
-                // Errors
-                (msg, conn) => {
-                    info!("Receive unexpected message. Expected block msg but receive {}", msg);
-                    info!("Drop connection {:?}", conn);
-                    Err(Error::from(ErrorKind::MisbehaviorPeer(conn)))
-                },
-            }
-        })
-    })
+                }
+
+                match is_completed {
+                    true => Ok(Loop::Break((conn, block_chain))),
+                    false => Ok(Loop::Continue((conn, block_chain))),
+                }
+            })
+        },
+    )
 }
