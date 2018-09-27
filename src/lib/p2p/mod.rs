e9017d603a64e65ae0b4239e0b690fdeb781eaf4
AtsukiTak <takatomgoo@gmail.com>
2018-09-27 12:29:11 +0900

    reexpose Connection and Socket as p2p module level

--- src/lib/p2p/mod.rs
+++ src/lib/p2p/mod.rs
@@ -1,5 +1,11 @@
-pub mod socket;
-pub mod connection;
+mod socket;
+mod connection;
 
 pub use self::socket::Socket;
 pub use self::connection::Connection;
+
+pub mod msg
+{
+    pub use self::connection::{BlockResponse, Disconnect, GetBlockRequest, GetHeadersRequest, HeadersResponse,
+                               PublishInv, SubscribeInv};
+}
